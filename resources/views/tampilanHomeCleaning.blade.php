7e2d470e2714c27741e06b2c28829a94d2245386
hariando7 <rianaja2200022@gmail.com>
2023-11-08 22:37:22 +0700

    comit ando update

--- resources/views/tampilanHomeCleaning.blade.php
+++ resources/views/tampilanHomeCleaning.blade.php
@@ -22,7 +22,9 @@
         class="mt-20 w-full h-[870px] bg-cover bg-no-repeat relative bg-[url({{asset('assets/tampilanAssets/ORGANIGRAM.png')}})]">
         <div class="lg:w-[40%] md:w-[70%] sm:w-full flex flex-col ml-16 mr-16 mb-20 text-start">
             <h1 class="lg:text-5xl md:text-3xl sm:text-xl font-bold text-white mt-20">OUR ORGANIGRAM</h1>
-            <p class="text-base font-thin text-white mt-4">ini merupakan organigram dari SweepMop Home Cleaning
+            <p class="text-base font-thin text-white mt-4">Ini adalah organigram dari SweepMop dan Home Cleaning yang
+                menunjukkan struktur organisasi dan bagaimana setiap bagian dalam perusahaan kami berperan dalam
+                memberikan layanan terbaik kepada pelanggan kami
             </p>
         </div>
         <div class="justify-items-center">
@@ -33,7 +35,9 @@
     <div class="w-full flex flex-col justify-center items-center mt-36 mb-36">
         <div class="w-[80%] bg-[#FA8F21] p-10 rounded-lg">
             <h1 class="lg:text-5xl md:text-4xl sm:text-3xl font-bold text-white text-center">WORKING COMPANIES</h1>
-            <p class="text-sm md:text-base font-thin text-white text-justify md:text-center mt-4 m-2 md:m-8 pl-0 md:pl-24 md:pr-24">Perusahaan yang
+            <p
+                class="text-sm md:text-base font-thin text-white text-justify md:text-center mt-4 m-2 md:m-8 pl-0 md:pl-24 md:pr-24">
+                Perusahaan yang
                 Bekerjasama dengan Home Cleaning dan Catering
             </p>
             <x-carouselWorkingCompanies />

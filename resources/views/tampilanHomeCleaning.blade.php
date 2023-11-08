0a928f43038c96a12584039cc3fe9dc6e54ba94d
hariando7 <rianaja2200022@gmail.com>
2023-11-08 22:36:11 +0700

    comit ando

--- resources/views/tampilanHomeCleaning.blade.php
+++ resources/views/tampilanHomeCleaning.blade.php
@@ -2,23 +2,27 @@
     <x-heroTampilan title="PT. ElyNaz IndoMasr Global "
         desc="PT. ElyNaz IndoMasr Global memberikan solusi layanan komprehensif yang disesuaikan dengan kinerja dan kebutuhan bisnis anda."
         ref="#section2" imgPath="{{asset('assets/tampilanAssets/cleaningBackground.png')}}" butTitle="Explore Now" />
-        <x-humberger refWA="https://api.whatsapp.com/send/?phone=081539274249&text&type=phone_number&app_absent=0" refInstagram="https://www.instagram.com/sweepmop/"/>
-        <div id="section2" class="w-full flex flex-col justify-center items-center">
-        <div class="w-[50%]">
-            <h1 class="text-5xl font-bold text-black text-center mt-20">OUR SERVICES</h1>
-            <p class="text-base font-thin text-black text-center mt-4">Lorem Ipsum is simply dummy text of the printing
-                and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+    <x-humberger refWA="https://api.whatsapp.com/send/?phone=081539274249&text&type=phone_number&app_absent=0"
+        refInstagram="https://www.instagram.com/sweepmop/" />
+    <div id="section2" class="w-full flex flex-col justify-center items-center">
+        <div class="w-[70%] md:w-[50%]">
+            <h1 class="text-xl md:text-5xl font-bold text-black text-center mt-20">OUR SERVICES</h1>
+            <p class="text-base font-thin text-black text-justify mt-4">Berdiri sejak tahun 2010, Sweepmop telah
+                berpengalaman di bidang pelayanan jasa, SweepMop menjadi solusi kebersihan yang tepat bagi kebutuhan
+                anda. Pilihan pembersihan yang beragam dapat disesuaikan dengan kebutuhan para pelanggan Sweepmop mulai
+                dari pembersihan Hunian, Kantor, Hotel, Rumah Sakit, Pabrik, Asrama, Sekolah, baik pembersihan harian
+                maupun perawatan berkala hingga pelayanan kebersihan khusus bagi industri tertentu
             </p>
         </div>
     </div>
     <div class="mt-14">
         <x-carouselService />
     </div>
-    <div class="mt-20 w-full h-[870px] bg-cover bg-no-repeat relative bg-[url({{asset('assets/tampilanAssets/ORGANIGRAM.png')}})]">
+    <div
+        class="mt-20 w-full h-[870px] bg-cover bg-no-repeat relative bg-[url({{asset('assets/tampilanAssets/ORGANIGRAM.png')}})]">
         <div class="lg:w-[40%] md:w-[70%] sm:w-full flex flex-col ml-16 mr-16 mb-20 text-start">
             <h1 class="lg:text-5xl md:text-3xl sm:text-xl font-bold text-white mt-20">OUR ORGANIGRAM</h1>
-            <p class="text-base font-thin text-white mt-4">Lorem Ipsum is simply dummy text of the printing and
-                typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+            <p class="text-base font-thin text-white mt-4">ini merupakan organigram dari SweepMop Home Cleaning
             </p>
         </div>
         <div class="justify-items-center">
@@ -29,10 +33,8 @@
     <div class="w-full flex flex-col justify-center items-center mt-36 mb-36">
         <div class="w-[80%] bg-[#FA8F21] p-10 rounded-lg">
             <h1 class="lg:text-5xl md:text-4xl sm:text-3xl font-bold text-white text-center">WORKING COMPANIES</h1>
-            <p class="text-base font-thin text-white text-center mt-4 m-8">Lorem Ipsum is simply dummy text of the
-                printing
-                and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the
-                1500s,
+            <p class="text-sm md:text-base font-thin text-white text-justify md:text-center mt-4 m-2 md:m-8 pl-0 md:pl-24 md:pr-24">Perusahaan yang
+                Bekerjasama dengan Home Cleaning dan Catering
             </p>
             <x-carouselWorkingCompanies />
 
--- resources/views/tampilanTentangPerusahaan.blade.php
+++ resources/views/tampilanTentangPerusahaan.blade.php
@@ -3,8 +3,7 @@
         desc="PT. ElyNaz IndoMasr Global memberikan solusi layanan komprehensif yang disesuaikan dengan kinerja dan kebutuhan bisnis anda."
         ref="#section2" imgPath="{{ asset('assets/tampilanAssets/bgTentangPerusahaan.png') }}" butTitle="Explore Now" />
     <x-humberger refWA="https://api.whatsapp.com/send/?phone=Hp082373377203
-&text&type=phone_number&app_absent=0"
-        refInstagram="https://www.instagram.com/elynazindomasr/" />
+&text&type=phone_number&app_absent=0" refInstagram="https://www.instagram.com/elynazindomasr/" />
     <!-- Mobile -->
     <div class="md:hidden">
         <div class="justify-center items-start bg-[#EEEFF4] pt-10">
@@ -12,25 +11,13 @@
                 <img src="assets/tampilanAssets/about.png" alt=""
                     class="w-[60%] h-[40%] flex justify-center items-center m-auto">
                 <div class="pl-8 pr-8 pb-8 text-justify ">
-                    <h1 class="text-5xl font-bold text-black pb-10 mt-10 text-center">ABOUT US</h1>
-                    <p class="text-lg font-thin text-black text-justify">
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
-                        been
-                        the
-                        industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
-                        of
-                        type
-                        and
-                        scrambled it to make a type specimen book. It has survived not only five centuries, but also
-                        the
-                        leap
-                        into electronic typesetting, remaining essentially unchanged. It was popularised in the
-                        1960s
-                        with
-                        the
-                        release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
-                        publishing
-                        software like Aldus PageMaker including versions of Lorem Ipsum
+                    <h1 class="text-3xl font-bold text-black pb-10 mt-10 text-center">ABOUT US</h1>
+                    <p class="text-sm font-thin text-black text-justify">
+                        PT. Elynaz Indomasr Global adalah perusahaan penyedia layanan yang bergerak dalam 2 bidang usaha
+                        yaitu bidang jasa boga dan jasa cleaning service. kami memiliki 2 devisi penyedia layanan
+                        terpercaya dibawah naungan PT. Elynaz Indomasr Global. Layanan yang bergerak dalam bidang jasa
+                        boga dengan merk Elynaz Catering dan layanan yang bergerak dalam bidang jasa cleaning service
+                        dengan merk SweepMop
                     </p>
                 </div>
             </div>
@@ -40,22 +27,25 @@
                 <img src="assets/tampilanAssets/hostoryCompany.png" alt=""
                     class="w-[60%] h-[40%] flex justify-center items-center m-auto">
                 <div class="pl-8 pr-8 pb-8 text-justify ">
-                    <h1 class="text-5xl font-bold text-black pb-10 mt-10 text-center">Company History</h1>
-                    <p class="text-lg font-thin text-black text-justify">
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
-                        been
-                        the
-                        industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
-                        of
-                        type
-                        and scrambled it to make a type specimen book. It has survived not only five centuries, but
-                        also
-                        the
-                        leap into electronic typesetting, remaining essentially unchanged. It was popularised in the
-                        1960s
-                        with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with
-                        desktop
-                        publishing software like Aldus PageMaker including versions of Lorem Ipsum
+                    <h1 class="text-3xl font-bold text-black pb-10 mt-10 text-center">Company History</h1>
+                    <p class="text-sm font-thin text-black text-justify mb-10">
+                        PT. Elynaz Indomasr Global adalah perusahaan penyedia layanan yang bergerak
+                        dalam 2 bidang usaha yaitu bidang jasa boga dan jasa cleaning service. Memiliki 2 devisi
+                        penyedia layanan terpercaya dibawah naungan PT. Elynaz Indomasr Global.. Elynaz Catering yang
+                        bergerak dalam bidang jasa boga dan Sweep Mop yang bergerak dalam bidang jasa cleaning service.
+
+                        Seiring berjalannya waktu PT. Elynaz Indomasr Global mulai meningkatkan kapasitas layanannya
+                        dengan mengikuti tender pengadaan jasa boga maupun jasa cleaning service (outsourcing) dari
+                        Pemerintah dan BUMN dalam jumlah besar.
+
+                        Kami memberikan layanan terintegrasi yang terbaik bagi kebutuhan boga maupun cleaning dalam
+                        bisnis anda, dengan sebelumnya memahami bagaimana bisnis anda berjalan dan apa saja yang
+                        dibutuhkan untuk mendukung efisiensi kegiatan utama bisnis anda.
+
+                        Dalam menjaga mutu kualitasnya, kami juga memberdayakan karyawan kami untuk mampu secara efisien
+                        bekerja melebihi ekspektasi, dan memberikan mereka kesempatan untuk mengeksplorasi layanan
+                        secara berkala dan memastikan hasil
+
                     </p>
                 </div>
             </div>
@@ -69,19 +59,8 @@
                             Visi Perusahaan
                         </h5>
                     </a>
-                    <p class="mb-3 font-normal text-white text-justify">
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
-                        been
-                        the
-                        industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
-                        of
-                        type
-                        and scrambled it to make a type specimen book. It has survived not only five centuries, but
-                        also
-                        the
-                        leap into electronic typesetting, remaining essentially unchanged. It was popularised in the
-                        1960s
-                        with the release of Letraset sheets containing Lorem Ipsum passages.
+                    <p class="mb-3 font-normal text-white text-justify text-sm">
+                        Menjadi perusahaan terkemuka dan pengembang layanan dengan kualitas terpercaya
                     </p>
                 </div>
                 <div class="max-w-sm p-6 bg-[#FA8F21] shadow rounded-tr-3xl">
@@ -90,26 +69,16 @@
                             Misi Perusahaan
                         </h5>
                     </a>
-                    <p class="mb-3 font-normal text-white text-justify">
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
-                        been
-                        the
-                        industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
-                        of
-                        type
-                        and scrambled it to make a type specimen book. It has survived not only five centuries, but
-                        also
-                        the
-                        leap into electronic typesetting, remaining essentially unchanged. It was popularised in the
-                        1960s
-                        with the release of Letraset sheets containing Lorem Ipsum passages.
+                    <p class="mb-3 font-normal text-white text-justify text-sm">
+                        Memberikan layanan kinerja terbaik sehingga menghasilkan kualitas, efektifitas, dan efisiensi
+                        pelayanan yang bertanggung jawab
                     </p>
                 </div>
             </div>
         </div>
-        <div class="w-full flex flex-col justify-center items-center ">
+        <div class="w-full flex flex-col justify-center items-center p-20 ">
             <div class="w-full">
-                <h1 class="text-5xl font-bold text-black text-center mt-20">CORPORATE STRUCTURE</h1>
+                <h1 class="text-3xl font-bold text-black text-center mt-20">CORPORATE STRUCTURE</h1>
             </div>
             <div class="w-full max-w-sm border rounded-lg shadow border-black mt-10">
                 <div class="flex flex-col items-center p-5 bg-[#001BB0] rounded-t-lg">
@@ -117,7 +86,7 @@
                         alt="Bonnie image" />
                 </div>
                 <div class="text-center">
-                    <h5 class="mb-1 text-xl font-medium text-[#000000]">Evialty Primelly</h5>
+                    <h5 class="mb-1 text-md font-medium text-[#000000]">Evialty Primelly</h5>
                     <span class="text-sm text-[#000000]">Direktur</span>
                 </div>
             </div>
@@ -128,19 +97,18 @@
                         alt="Ceisa Shadrina Pranindira, S.E" />
                 </div>
                 <div class="text-center">
-                    <h5 class="mb-1 text-xl font-medium text-[#000000]">Ceisa Shadrina Pranindira, S.E</h5>
+                    <h5 class="mb-1 text-md font-medium text-[#000000]">Ceisa Shadrina <br> Pranindira, S.E</h5>
                     <span class="text-sm text-[#000000]">Marketing Manager</span>
                 </div>
             </div>
             <div class="w-0.5 h-[39px] bg-black"></div>
             <div class="w-full max-w-sm border rounded-lg shadow border-black">
                 <div class="flex flex-col items-center p-5 bg-[#001BB0] rounded-t-lg">
-                    <img class="w-36 h-36 rounded-full shadow-lg" src="assets/tampilanAssets/waelProfile.png"
-                        alt="Wael Shoukry Ahmed
+                    <img class="w-36 h-36 rounded-full shadow-lg" src="assets/tampilanAssets/waelProfile.png" alt="Wael Shoukry Ahmed
 Abdalla Aboelnaga" />
                 </div>
                 <div class="text-center">
-                    <h5 class="mb-1 text-xl font-medium text-[#000000]">Wael Shoukry Ahmed <br />
+                    <h5 class="mb-1 text-md font-medium text-[#000000]">Wael Shoukry Ahmed <br />
                         Abdalla Aboelnaga</h5>
                     <span class="text-sm text-[#000000]">Direktur Usaha</span>
                 </div>
@@ -158,23 +126,11 @@ Abdalla Aboelnaga" />
                 <div class="pl-16">
                     <h1 class="text-5xl font-bold text-black pb-10">ABOUT US</h1>
                     <p class="text-lg font-thin text-black text-justify">
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
-                        been
-                        the
-                        industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
-                        of
-                        type
-                        and
-                        scrambled it to make a type specimen book. It has survived not only five centuries, but also
-                        the
-                        leap
-                        into electronic typesetting, remaining essentially unchanged. It was popularised in the
-                        1960s
-                        with
-                        the
-                        release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
-                        publishing
-                        software like Aldus PageMaker including versions of Lorem Ipsum
+                        PT. Elynaz Indomasr Global adalah perusahaan penyedia layanan yang bergerak dalam 2 bidang usaha
+                        yaitu bidang jasa boga dan jasa cleaning service. kami memiliki 2 devisi penyedia layanan
+                        terpercaya dibawah naungan PT. Elynaz Indomasr Global. Layanan yang bergerak dalam bidang jasa
+                        boga dengan merk Elynaz Catering dan layanan yang bergerak dalam bidang jasa cleaning service
+                        dengan merk SweepMop
                     </p>
                 </div>
             </div>
@@ -185,23 +141,27 @@ Abdalla Aboelnaga" />
                     <h1 class="text-5xl font-bold text-black pb-10 w-[10%]">Company
                         History</h1>
                     <p class="text-lg font-thin text-black text-justify w-[90%]">
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
-                        been
-                        the
-                        industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
-                        of
-                        type
-                        and scrambled it to make a type specimen book. It has survived not only five centuries, but
-                        also
-                        the
-                        leap into electronic typesetting, remaining essentially unchanged. It was popularised in the
-                        1960s
-                        with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with
-                        desktop
-                        publishing software like Aldus PageMaker including versions of Lorem Ipsum
+                        PT. Elynaz Indomasr Global adalah perusahaan penyedia layanan yang bergerak
+                        dalam 2 bidang usaha yaitu bidang jasa boga dan jasa cleaning service. Memiliki 2 devisi
+                        penyedia layanan terpercaya dibawah naungan PT. Elynaz Indomasr Global.. Elynaz Catering yang
+                        bergerak dalam bidang jasa boga dan Sweep Mop yang bergerak dalam bidang jasa cleaning service.
+
+                        Seiring berjalannya waktu PT. Elynaz Indomasr Global mulai meningkatkan kapasitas layanannya
+                        dengan mengikuti tender pengadaan jasa boga maupun jasa cleaning service (outsourcing) dari
+                        Pemerintah dan BUMN dalam jumlah besar.
+
+                        Kami memberikan layanan terintegrasi yang terbaik bagi kebutuhan boga maupun cleaning dalam
+                        bisnis anda, dengan sebelumnya memahami bagaimana bisnis anda berjalan dan apa saja yang
+                        dibutuhkan untuk mendukung efisiensi kegiatan utama bisnis anda.
+
+                        Dalam menjaga mutu kualitasnya, kami juga memberdayakan karyawan kami untuk mampu secara efisien
+                        bekerja melebihi ekspektasi, dan memberikan mereka kesempatan untuk mengeksplorasi layanan
+                        secara berkala dan memastikan hasil
+
                     </p>
                 </div>
-                <img src="assets/tampilanAssets/hostoryCompany.png" alt="" class="w-[50%] h-[40%]">
+                <img src="assets/tampilanAssets/hostoryCompany.png" alt=""
+                    class="w-[50%] h-[40%] m-auto flex items-center justify-content-center">
             </div>
         </div>
         <div
@@ -214,18 +174,7 @@ Abdalla Aboelnaga" />
                         </h5>
                     </a>
                     <p class="mb-3 font-normal text-white text-justify">
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
-                        been
-                        the
-                        industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
-                        of
-                        type
-                        and scrambled it to make a type specimen book. It has survived not only five centuries, but
-                        also
-                        the
-                        leap into electronic typesetting, remaining essentially unchanged. It was popularised in the
-                        1960s
-                        with the release of Letraset sheets containing Lorem Ipsum passages.
+                        Menjadi perusahaan terkemuka dan pengembang layanan dengan kualitas terpercaya
                     </p>
                 </div>
                 <div class="max-w-sm p-6 bg-[#FA8F21] shadow rounded-tr-3xl">
@@ -235,18 +184,8 @@ Abdalla Aboelnaga" />
                         </h5>
                     </a>
                     <p class="mb-3 font-normal text-white text-justify">
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
-                        been
-                        the
-                        industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
-                        of
-                        type
-                        and scrambled it to make a type specimen book. It has survived not only five centuries, but
-                        also
-                        the
-                        leap into electronic typesetting, remaining essentially unchanged. It was popularised in the
-                        1960s
-                        with the release of Letraset sheets containing Lorem Ipsum passages.
+                        Memberikan layanan kinerja terbaik sehingga menghasilkan kualitas, efektifitas, dan efisiensi
+                        pelayanan yang bertanggung jawab
                     </p>
                 </div>
             </div>
@@ -279,8 +218,7 @@ Abdalla Aboelnaga" />
             <div class="w-0.5 h-[39px] bg-black"></div>
             <div class="w-full max-w-sm border rounded-lg shadow border-black">
                 <div class="flex flex-col items-center p-5 bg-[#001BB0] rounded-t-lg">
-                    <img class="w-36 h-36 rounded-full shadow-lg" src="assets/tampilanAssets/waelProfile.png"
-                        alt="Wael Shoukry Ahmed
+                    <img class="w-36 h-36 rounded-full shadow-lg" src="assets/tampilanAssets/waelProfile.png" alt="Wael Shoukry Ahmed
 Abdalla Aboelnaga" />
                 </div>
                 <div class="text-center">
@@ -295,10 +233,9 @@ Abdalla Aboelnaga" />
     <div class="w-full flex flex-col justify-center items-center mt-20 mb-24">
         <div class="w-[80%] bg-[#FA8F21] p-10 rounded-lg">
             <h1 class="lg:text-5xl md:text-4xl sm:text-3xl font-bold text-white text-center">WORKING COMPANIES</h1>
-            <p class="text-base font-thin text-white text-center mt-4 m-8">Lorem Ipsum is simply dummy text of the
-                printing
-                and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the
-                1500s,
+            <p class="text-sm md:text-base font-thin text-white text-justify md:text-center mt-4 m-2 md:m-8 pl-0 md:pl-24 md:pr-24">Perusahaan yang Bekerjasama dengan Home Cleaning dan Catering
+
+
             </p>
             <x-carouselWorkingCompanies />
         </div>
